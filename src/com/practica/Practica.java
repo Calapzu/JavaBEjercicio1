c2fd17eb409c791540c9c1d63729672b00b71f7e
Calapzu <yamsoncalapzu@gmail.com>
2022-03-27 03:24:48 -0500

    Se creamn los tipos de datos mas comunes y se imprimen

--- src/com/practica/Practica.java
+++ src/com/practica/Practica.java
@@ -1,4 +1,22 @@
 package com.practica;
 
 public class Practica {
+    public static void main(String[] args) {
+
+        int numeroEntero = 1;
+        long numeroLong = 2;
+        double numeroDouble = 3.0;
+
+        boolean variableFalsa = false;
+        boolean variableVerdadera = true;
+
+        String variableTexto = "Hola estoy en el OpenBootCamp";
+
+        System.out.println("Numero entero: "+numeroEntero
+        +"\nNumero Long: "+ numeroLong
+        +"\nNumero Double: "+numeroDouble
+        +"\nVariable Falsa: "+variableFalsa
+        +"\nVariable Verdadera: "+variableVerdadera
+        +"\nVariable Texto: "+variableTexto);
+    }
 }
